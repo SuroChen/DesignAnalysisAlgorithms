a1d2abaa080230b65ff7d768ff737e62c7570ad7
Suro <21019515@qq.com>
2019-07-26 10:47:52 +0800

    实现获取整数序列的逆序数算法

--- src/com/suro/main/Main.java
+++ src/com/suro/main/Main.java
@@ -6,9 +6,10 @@ public class Main {
     public static void main(String[] args) {
         int[] a = {5, 1, 9, 4, 6, 2, 0, 3, 8, 7};
         int i;
-        Sort.bubbleSort(a);
-        for (i = 0; i < a.length; i++)
-            System.out.print(a[i] + " ");
+        int count = Sort.getTheInvertion(a);
+//        for (i = 0; i < a.length; i++)
+//            System.out.print(a[i] + " ");
+        System.out.print(count);
         System.out.println();
     }
 }
--- src/com/suro/sort/Sort.java
+++ src/com/suro/sort/Sort.java
@@ -14,6 +14,21 @@ public class Sort {
         }
     }
 
+    public static int getTheInvertion(int[] a) {
+        int i, j, key, n = a.length, count = 0;
+        for (j = 1; j < n; j++) {
+            key = a[j];
+            i = j - 1;
+            while (i >= 0 && a[i] > key) {
+                a[i + 1] = a[i];
+                i--;
+                count++;
+            }
+            a[i + 1] = key;
+        }
+        return count;
+    }
+
     public static void bubbleSort(int[] a) {
         int i, j, key, n = a.length;
         for (i = 0; i < n; i++) {
